22ffe947401cc660991a29a41dd118694ae682ea
Marcin <91530764+Mgalazyn@users.noreply.github.com>
2022-07-12 19:23:37 +0200

    Delete My_cinema directory

--- My_cinema/My_cinema/main.py
+++
@@ -1,9 +0,0 @@
-from new_movies.actions import user as user_actions
-
-
-def run_example():
-    user_actions.login()
-
-
-if __name__ == "__main__":
-    run_example()
--- My_cinema/new_movies/configuration.py
+++
@@ -1,4 +0,0 @@
-from datetime import date
-
-UNLIMITED_WATCHING_START_DATE = date(year=2022, month=2, day=1)
-UNLIMITED_WATCHING_END_DATE = date(year=2022, month=9, day=30)
